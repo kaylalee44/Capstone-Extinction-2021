a36c3f41f29521703db551b2eb765fdca6fc1279
Kayla Lee <45502276+kaylalee44@users.noreply.github.com>
2021-04-08 00:02:03 -0700

    Worked on footer and details, need to do css

--- project-perception/src/App.js
+++ project-perception/src/App.js
@@ -2,9 +2,9 @@ import React, {useEffect, useState} from "react";
 import './index.css'
 import * as ROUTES from "./constants/routes"; 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import StoriesStart from "./pages/stories_start";
-import Stories from './pages/stories';
+import StoriesPage from './pages/stories';
 import { NavBar } from './components/navbar/navbar';
+import Footer from './components/footer/index';
 
 function App() {
   return (
@@ -15,11 +15,11 @@ function App() {
       <main>
         <Router>
           <Switch>
-            <Route path={ROUTES.STORIES_START} component={StoriesStart} />
-            <Route path={ROUTES.STORIES} component={Stories} />
+            <Route path={ROUTES.STORIES} component={StoriesPage} />
           </Switch>
         </Router>
       </main>
+      <Footer />
     </div>
   );
 }
--- project-perception/src/constants/routes.js
+++ project-perception/src/constants/routes.js
@@ -2,5 +2,4 @@ export const HOME = "/";
 export const ABOUT = "/about";
 export const CHANGE = "/change";
 export const STORIES = "/stories"
-export const STORIES_START = "/stories-start";
 export const STORY_TURTLE = "/story-turtle";
